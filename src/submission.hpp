7aa9e37a91aad11f9a857d54bfa26dbe4ec20807
unknown <sharon@basovich.com>
2026-08-25 13:42:04 -0400

    using pointers, improvement unclear

--- src/submission.hpp
+++ src/submission.hpp
@@ -41,26 +41,67 @@ public:
   {
     return cols_;
   }
+
+  const double *base() const
+  {
+    return data.data();
+  }
+
+  double *base()
+  {
+    return data.data();
+  }
 };
 
 // Apply the five-point stencil over all interior points, copying the boundary
 // values unchanged from old_grid to new_grid. Implement your solution here.
+// void apply_stencil(const Grid &old_grid, Grid &new_grid)
+// {
+//   std::size_t rows = old_grid.rows();
+//   std::size_t cols = old_grid.cols();
+//   for (std::size_t i = 1; i < rows - 1; i++)
+//   {
+//     for (std::size_t j = 1; j < cols - 1; j++)
+//     {
+//       new_grid(i, j) = 0.5 * old_grid(i, j) + 0.125 * (old_grid(i - 1, j) + old_grid(i, j - 1) + old_grid(i + 1, j) + old_grid(i, j + 1));
+
+//       // potential scatter strategy to look at later
+//       // new_grid(i, j) += old_grid(i, j) * 0.5;
+//       // new_grid(i, j + 1) += old_grid(i, j) * 0.125;
+//       // new_grid(i, j - 1) += old_grid(i, j) * 0.125;
+//       // new_grid(i + 1, j) += old_grid(i, j) * 0.125;
+//       // new_grid(i - 1, j) += old_grid(i, j) * 0.125;
+//     }
+//   }
+
+//   for (std::size_t i = 0; i < rows; i++)
+//   {
+//     new_grid(i, 0) = old_grid(i, 0);
+//     new_grid(i, cols - 1) = old_grid(i, cols - 1);
+//   }
+
+//   for (std::size_t i = 0; i < cols; i++)
+//   {
+//     new_grid(0, i) = old_grid(0, i);
+//     new_grid(rows - 1, i) = old_grid(rows - 1, i);
+//   }
+// }
+
 void apply_stencil(const Grid &old_grid, Grid &new_grid)
 {
+  const double *old_base = old_grid.base();
+  double *new_base = new_grid.base();
   std::size_t rows = old_grid.rows();
   std::size_t cols = old_grid.cols();
   for (std::size_t i = 1; i < rows - 1; i++)
   {
+    const double *curr = old_base + i * cols;
+    const double *prev = old_base + (i - 1) * cols;
+    const double *next = old_base + (i + 1) * cols;
+    double *new_cell = new_base + i * cols;
     for (std::size_t j = 1; j < cols - 1; j++)
     {
-      new_grid(i, j) = 0.5 * old_grid(i, j) + 0.125 * (old_grid(i - 1, j) + old_grid(i, j - 1) + old_grid(i + 1, j) + old_grid(i, j + 1));
-      
-      // potential scatter strategy to look at later
-      // new_grid(i, j) += old_grid(i, j) * 0.5;
-      // new_grid(i, j + 1) += old_grid(i, j) * 0.125;
-      // new_grid(i, j - 1) += old_grid(i, j) * 0.125;
-      // new_grid(i + 1, j) += old_grid(i, j) * 0.125;
-      // new_grid(i - 1, j) += old_grid(i, j) * 0.125;
+      new_cell[j] = 0.5 * curr[j] + 0.125 * (curr[j + 1] + curr[j - 1] + prev[j] + next[j]);
     }
   }
 
