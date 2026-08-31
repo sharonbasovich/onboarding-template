b7b3e6cbf7e183f0e05ae27b7157a01ddbf5f705
unknown <sharon@basovich.com>
2026-08-31 13:47:39 -0400

    spinning on low thread count

--- src/submission.hpp
+++ src/submission.hpp
@@ -139,7 +139,7 @@ public:
     // worker_count_ = hardware_threads > 1 ? hardware_threads - 1 : 0;
 
     // TESTING ONLY
-    worker_count_ = 2;
+    worker_count_ = 3;
 
     workers_.reserve(worker_count_);
 
