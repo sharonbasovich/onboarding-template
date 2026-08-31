d0190d131718c4bce67279f1e35b713866932a8f
unknown <sharon@basovich.com>
2026-08-31 12:59:31 -0400

    testing thread counts v3

--- src/submission.hpp
+++ src/submission.hpp
@@ -117,7 +117,7 @@ public:
     worker_count_ = hardware_threads > 1 ? hardware_threads - 1 : 0;
 
     // TESTING ONLY
-    worker_count_ = 7;
+    worker_count_ = 15;
 
     workers_.reserve(worker_count_);
 
